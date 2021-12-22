2a80741926d771ed96e25a243e3670bd3f341b52
HitriyiDmitry <95777128+HitriyiDmitry@users.noreply.github.com>
2021-12-22 14:42:59 +0300

    Revert "Add files via upload"
    This reverts commit 32c6f7fdd46af2750c97864c8fa1b48a828768ae.

--- task_3_1.py
+++
@@ -1,7 +0,0 @@
-my_translate = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть',
-                'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
-
-def num_translate(english):
-    return my_translate.get(english)
-
-print(num_translate(input('Ввкдите прописью число от 0 до 10 на английском: ')))
